6604b52fdecf95ac1911f467eb7647f5e027e3f1
arnon3339 <arnon3339@gmail.com>
2025-01-29 00:10:20 +0700

    disable eslint line rule

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(_req: NextRequest) {
     return NextResponse.json({
         message: "Hello!"
--- app/api/text/route.ts
+++ app/api/text/route.ts
@@ -1,11 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function GET(_req: NextRequest) {
     return NextResponse.json({
         message: "Hello!"
     });
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function POST(_req: NextRequest) {
     const allEmojis = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🦝", "🐻", "🐼", "🐨", "🐯",
          "🦁", "🐮", "🐷", "🐽", "🐸", "🐵", "🙈", "🙉", "🙊", "🐒", "🦍", "🦧", "🦮", "🐕",
